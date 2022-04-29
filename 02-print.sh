c2a8687847cd05eea6e348d9475f8fb870030cf5
Win10 <yeddulaparineetha@gmail.com>
2022-04-29 23:26:46 +0530

    Usage of \n in the shell script

--- 02-print.sh
+++ 02-print.sh
@@ -1,2 +1,14 @@
 #! /usr/bin/bash
 echo Hello World
+
+# Multi line printing
+# syntax : echo -e "line1\nline2"
+
+## -e option is needed for \n to work
+## "" -> Double quite / single quote is needed for \n to work
+## \n -> Print a new line
+
+echo -e "Hello\nWorld"
+
+# Printing with colors
+
